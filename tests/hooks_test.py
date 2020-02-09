520badd52b079173278459d12ebb991f77d4c89c
Anthony Sottile <asottile@umich.edu>
2020-02-08 22:47:45 -0800

    Allow additional text in type ignores

--- tests/hooks_test.py
+++ tests/hooks_test.py
@@ -10,7 +10,7 @@ HOOKS = {h['id']: re.compile(h['entry']) for h in load_manifest(MANIFEST_FILE)}
 @pytest.mark.parametrize(
     's',
     (
-        'x = 1 # type: ignore_me',
+        'x = 1 # type: ignoreme',
         'x = 1  # type: int',
         'x = 1  # type int',
         'x = 1  # type: int  # noqa',
@@ -29,6 +29,8 @@ def test_python_use_type_annotations_positive(s):
         'x = 1  # type:  ignore',
         'x = 1  # type: ignore # noqa',
         'x = 1  # type: ignore  # noqa',
+        'x = 1  # type: ignore[type-mismatch]',
+        'x = 1  # type: ignore=E123',
     ),
 )
 def test_python_use_type_annotations_negative(s):
