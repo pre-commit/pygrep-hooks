c2f162a73254f23f35607ecfbdae1e47a34496a5
Raphael Boidol <boidolr@users.noreply.github.com>
2020-10-10 14:13:38 +0200

    Add a few checks for typescript

--- tests/hooks_test.py
+++ tests/hooks_test.py
@@ -209,3 +209,97 @@ def test_text_unicode_replacement_char_positive(s):
 )
 def test_text_unicode_replacement_char_negative(s):
     assert not HOOKS['text-unicode-replacement-char'].search(s)
+
+
+@pytest.mark.parametrize(
+    's',
+    (
+        'tap(() => {debugger}),',
+        'foo(); debugger;',
+    ),
+)
+def test_ts_no_debugger_positive(s):
+    assert HOOKS['ts-no-debugger'].search(s)
+
+
+@pytest.mark.parametrize(
+    's',
+    (
+        'debug("function is ok")',
+    ),
+)
+def test_ts_no_debugger_negative(s):
+    assert not HOOKS['ts-no-debugger'].search(s)
+
+
+@pytest.mark.parametrize(
+    's',
+    (
+        'tap((args) => console.log(args)),',
+        'console.dir(obj);',
+        'console.trace(data)',
+        'console.info(data)',
+        'console.warn(data)',
+        'console.error(data)',
+    ),
+)
+def test_ts_no_console_positive(s):
+    assert HOOKS['ts-no-console'].search(s)
+
+
+@pytest.mark.parametrize(
+    's',
+    (
+        'consoling("is ok");',
+    ),
+)
+def test_ts_no_console_negative(s):
+    assert not HOOKS['ts-no-console'].search(s)
+
+
+@pytest.mark.parametrize(
+    's',
+    (
+        'foo(window);',
+        'window.location = "/foo"',
+        'window.onload = function() {}',
+    ),
+)
+def test_ts_no_window_positive(s):
+    assert HOOKS['ts-no-window'].search(s)
+
+
+@pytest.mark.parametrize(
+    's',
+    (
+        'const windows = ["is ok"];',
+    ),
+)
+def test_ts_no_window_negative(s):
+    assert not HOOKS['ts-no-window'].search(s)
+
+
+@pytest.mark.parametrize(
+    's',
+    (
+        'fdescribe("focus block", () => ({}))',
+        'xdescribe("ignore block", () => ({}))',
+        'xdescribe ("ignore block with space", () => ({}))',
+        'fit("focus case", () => ({}))',
+        'xit("ignore case", () => ({}))',
+        'xit ("ignore case with space", () => ({}))',
+    ),
+)
+def test_ts_no_focus_ignore_positive(s):
+    assert HOOKS['ts-no-focus-ignore'].search(s)
+
+
+@pytest.mark.parametrize(
+    's',
+    (
+        'describe("block", () => {',
+        'it("case", () => {',
+    ),
+)
+def test_ts_no_focus_ignore_negative(s):
+    assert not HOOKS['ts-no-focus-ignore'].search(s)
